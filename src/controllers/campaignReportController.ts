cd20fa36dba228efa6a7d68b819f6fde5f1e2dc3
Nicolás Condoluci <condoluci.nico@gmail.com>
2024-12-19 22:48:41 -0300

    Algunos cambios

--- src/controllers/campaignReportController.ts
+++ src/controllers/campaignReportController.ts
@@ -20,7 +20,7 @@ export const getCampaignReport = async (req: Request, res: Response, next: NextF
         return sendResponse(res, 200, {
             success: true,
             message: 'Report found',
-            data: report
+            report
         });
     } catch (error) {
         next(new AppError({message: "Error getting campaign report.", statusCode: 500, isOperational: false, data: error}));
@@ -29,7 +29,6 @@ export const getCampaignReport = async (req: Request, res: Response, next: NextF
 
 export const getCampaignReports = async (req: Request, res: Response, next: NextFunction) => {
     const { uid: campaignId} = req.params;
-    logger.info(campaignId)
 
     try {
         const reports = await CampaignReport.findAll({ where: { campaign_id: campaignId } });
